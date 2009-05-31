442a81338ce36d62692144531229d05afd76770c
Mitsunori Komatsu <komamitsu@gmail.com>
2009-06-01 01:55:46 +0900

    fixed cons's bug.

--- skewBinaryRandomAccessList.ml
+++ skewBinaryRandomAccessList.ml
@@ -5,7 +5,7 @@ let empty = []
 let is_empty = function [] -> true | _ -> false
   
 let cons x = function
-  | (w1, t1)::(w2, t2)::rest when t1 = t2
+  | (w1, t1)::(w2, t2)::rest when w1 = w2
     -> (1 + w1 + w2, Node (x, t1, t2))::rest
   | rl -> (1, Leaf x)::rl
 
@@ -27,7 +27,7 @@ let rec lookup_tree i = function
       if i = 0 then x
       else 
         if i < w / 2 then lookup_tree (i - 1) (w / 2,  t1)
-        else lookup_tree ((i - 1) - w / 2) (w / 2, t2)
+        else lookup_tree (i - 1 - w / 2) (w / 2, t2)
 
 let rec lookup i = function
   | [] -> raise (Invalid_argument "lookup: empty")
@@ -43,7 +43,7 @@ let rec update_tree i v = function
       if i = 0 then Node (v, t1, t2)
       else 
         if i < w / 2 then Node (x, (update_tree (i - 1) v (w / 2,  t1)), t2)
-        else Node (x, t1, (update_tree ((i - 1) - w / 2) v (w / 2, t2)))
+        else Node (x, t1, (update_tree (i - 1 - w / 2) v (w / 2, t2)))
 
 let rec update i v = function
   | [] -> raise (Invalid_argument "update: empty")
